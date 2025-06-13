3cd6fee650adb2c2bdcd22b4e9d81bec93a1df4c
Paras Mani Paterya <parasmani152@gmail.com>
2025-06-13 12:16:34 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+      <Route path = '/' element={<Home/>} /> 
+      <Route path = '/blog:id' element={<Blog/>} /> 
+
+
+      </Routes>
+      
+      
+      </div>
+  )
+}
+
+export default App
+
+
+//22:42
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import BlogList from '../components/BlogList'
+import Newsletter from '../components/Newsletter'
+
+const Home = () => {
+  return (
+    <>
+        
+
+      <Navbar/>
+      <Header/>
+      <BlogList/>
+      <Newsletter/>
+    </>
+  )
+}
+
+//1:08:36
+
+export default Home
