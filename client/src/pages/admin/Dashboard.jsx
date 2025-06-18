e01da04eb43b27822d1e2f9eae846902aefa2c8f
Paras Mani Paterya <parasmani152@gmail.com>
2025-06-18 19:46:24 +0530

    Fourth B  commit

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,42 +1,122 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
+import React from "react";
+import { assets, dashboard_data } from "../../assets/assets";
+import { useEffect } from "react";
+import BlogTableItem from "../../components/admin/BlogTableItem";
 
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = React.useState({
+    blogs: 0,
+    comments: 0,
+    drafs: 0,
+    recentBlogs: [],
+  });
 
-const [dashboardData, setDashboardData] = React.useState({ 
- 
-        blogs : 0,
-        comments : 0,
-        drafs : 0,  
-        recentBlogs : [],
+  // Simulating fetching data from an API
 
+  const fetchDashboard = async () => {
+    setDashboardData(dashboard_data);
+  };
 
- })
-
- // Simulating fetching data from an API
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
 
   return (
-    <div  className='flex-1 p-4 md:p-10 bg-blue-50/50'>
+    <div className="flex-1 p-4 md:p-10 bg-blue-50/50">
+      <div className="flex flex-wrap gap-4">
+        <div
+          className="flex items-center gap-4
+         bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all "
+        >
+          <img src={assets.dashboard_icon_1} alt="" />
 
-        <div className='flex flex-wrap gap-4' >
-
-            <div>
-
-                <img src={assets.dashboard_icon_1} alt="" />
-
-                <div>
-
-                    <p>{}</p>
-                </div>
-            </div>
-
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.blogs}
+            </p>
+            <p className="text-gray-400 font-light"> Blogs </p>
+          </div>
         </div>
 
+        <div
+          className="flex items-center gap-4
+         bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all "
+        >
+          <img src={assets.dashboard_icon_2} alt="" />
+
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.comments}
+            </p>
+            <p className="text-gray-400 font-light"> Comments </p>
+          </div>
+        </div>
+
+        <div
+          className="flex items-center gap-4
+         bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all "
+        >
+          <img src={assets.dashboard_icon_3} alt="" />
+
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.drafts}
+            </p>
+            <p className="text-gray-400 font-light"> Drafts </p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center gap-3 m-4 mt-6 text-gray-600">
+          <img src={assets.dashboard_icon_4} alt="" />
+          <p>Latest Blogs</p>
+        </div>
+
+        <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-xs text-gray-600 text-left uppercase">
+              <tr>
+                <th scope="col" className="px-2 py-4 xl:px-6">
+                  {" "}
+                  #{" "}
+                </th>
+                <th scope="col" className="px-2 py-4 ">
+                  {" "}
+                  Blog Title{" "}
+                </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden ">
+                  {" "}
+                  Date{" "}
+                </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden ">
+                  {" "}
+                  Status{" "}
+                </th>
+                <th scope="col" className="px-2 py-4 ">
+                  {" "}
+                  Actions{" "}
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {dashboardData.recentBlogs.map((blog, index) => {
+                return (
+                  <BlogTableItem
+                    key={blog._id}
+                    blog={blog}
+                    fetchBlogs={fetchDashboard}
+                    index={index + 1}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
-
-
-// 2:26:38
+export default Dashboard;
